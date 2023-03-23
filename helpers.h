1ff26a6ac23efbae94562694f65802f3100e395e
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-03-23 10:31:51 +0200

    added calc_blur_rgb to helpers.h

--- helpers.h
+++ helpers.h
@@ -12,3 +12,5 @@ void edges(int height, int width, RGBTRIPLE image[height][width]);
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width]);
 
+// Calculate blur rgb values for image[i][j] based on its neighbours
+RGBTRIPLE calc_blur_rgb(int count, ...);
